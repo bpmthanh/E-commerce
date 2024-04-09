b04f43e4bfd78b452779aeed34daeea35a3eb8a7
bpmthanhvinorsoft <thanhbpm@vinorsoft.com>
2024-04-09 15:17:16 +0700

    Fix bug danh mục

--- e-commerce-backend/src/main/java/com/bpmthanh/ecommercebackend/service/JWTService.java
+++ e-commerce-backend/src/main/java/com/bpmthanh/ecommercebackend/service/JWTService.java
@@ -15,40 +15,62 @@ import java.util.Date;
 @Service
 public class JWTService {
 
-    /** The secret key to encrypt the JWTs with. */
-    @Value("${jwt.algorithm.key}")
-    private String algorithmKey;
-    /** The issuer the JWT is signed with. */
-    @Value("${jwt.issuer}")
-    private String issuer;
-    /** How many seconds from generation should the JWT expire? */
-    @Value("${jwt.expiryInSeconds}")
-    private int expiryInSeconds;
-    /** The algorithm generated post construction. */
-    private Algorithm algorithm;
-    /** The JWT claim key for the username. */
-    private static final String USERNAME_KEY = "USERNAME";
+  /** The secret key to encrypt the JWTs with. */
+  @Value("${jwt.algorithm.key}")
+  private String algorithmKey;
+  /** The issuer the JWT is signed with. */
+  @Value("${jwt.issuer}")
+  private String issuer;
+  /** How many seconds from generation should the JWT expire? */
+  @Value("${jwt.expiryInSeconds}")
+  private int expiryInSeconds;
+  /** The algorithm generated post construction. */
+  private Algorithm algorithm;
+  /** The JWT claim key for the username. */
+  private static final String USERNAME_KEY = "USERNAME";
+  private static final String EMAIL_KEY = "EMAIL";
 
-    /**
-     * Post construction method.
-     */
-    @PostConstruct
-    public void postConstruct() {
-        algorithm = Algorithm.HMAC256(algorithmKey);
-    }
+  /**
+   * Post construction method.
+   */
+  @PostConstruct
+  public void postConstruct() {
+    algorithm = Algorithm.HMAC256(algorithmKey);
+  }
 
-    /**
-     * Generates a JWT based on the given user.
-     * 
-     * @param user The user to generate for.
-     * @return The JWT.
-     */
-    public String generateJWT(LocalUser user) {
-        return JWT.create()
-                .withClaim(USERNAME_KEY, user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + (1000 * expiryInSeconds)))
-                .withIssuer(issuer)
-                .sign(algorithm);
-    }
+  /**
+   * Generates a JWT based on the given user.
+   * @param user The user to generate for.
+   * @return The JWT.
+   */
+  public String generateJWT(LocalUser user) {
+    return JWT.create()
+        .withClaim(USERNAME_KEY, user.getUsername())
+        .withExpiresAt(new Date(System.currentTimeMillis() + (1000 * expiryInSeconds)))
+        .withIssuer(issuer)
+        .sign(algorithm);
+  }
+
+  /**
+   * Generates a special token for verification of an email.
+   * @param user The user to create the token for.
+   * @return The token generated.
+   */
+  public String generateVerificationJWT(LocalUser user) {
+    return JWT.create()
+        .withClaim(EMAIL_KEY, user.getEmail())
+        .withExpiresAt(new Date(System.currentTimeMillis() + (1000 * expiryInSeconds)))
+        .withIssuer(issuer)
+        .sign(algorithm);
+  }
+
+  /**
+   * Gets the username out of a given JWT.
+   * @param token The JWT to decode.
+   * @return The username stored inside.
+   */
+  public String getUsername(String token) {
+    return JWT.decode(token).getClaim(USERNAME_KEY).asString();
+  }
 
 }
